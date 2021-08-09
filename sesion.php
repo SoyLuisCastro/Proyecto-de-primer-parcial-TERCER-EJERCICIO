548a03fcd54bfd788b390958d97de28c2882fa57
SoyLuisCastro <86686595+SoyLuisCastro@users.noreply.github.com>
2021-08-09 12:13:05 -0500

    Add files via upload

---
+++ sesion.php
@@ -0,0 +1,16 @@
+<center><body background="https://data.1freewallpapers.com/download/lovely-flowers-in-spring-1366x768.jpg"></CENTER>
+<center>
+    <?php
+    $var = 1;
+    echo "El Valor Ingresado es: " . $var++ . "<br />\n";
+    echo "<h1>EL VALOR AUMENTO A:  $var</h1>";
+    $var = 1 + $var;
+    ?>
+    <button><span><a href="session.php">Aumentar El Valor</a></button></span><br>
+<br>
+<button><span> <a href="sesion2.php">Disminuir El Valor</a></span></button>
+
+
+
+
+</center>
---
+++ sesion2.php
@@ -0,0 +1,12 @@
+<center><body background="https://data.1freewallpapers.com/download/lovely-flowers-in-spring-1366x768.jpg"></CENTER>
+<center>
+    <?php
+    $var = 0;
+    echo "El Valor Ingresado Es: " . $var-- . "<br />\n";
+    echo "<h1>EL VALOR DISMINUYO A:  $var</h1>";
+    $var = 1-$var;
+    ?>
+    <button><span><a href="session.php">Aumentar El Valor</a></button></span><br>
+<br>
+<button><span> <a href="sesion2.php">Disminuir El Valor</a></span></button>
+</center>
